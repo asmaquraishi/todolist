5bebc28445d701837caceacbd1c9393d9bc3ffaa
asmaquraishi <asmaq66@gmail.com>
2019-02-20 16:05:33 +0300

    Updates

---
+++ precache-manifest.b8ecf320c31220a0442435910662c6bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4417f4c1c33ec024fd02",
+    "url": "/todolist/static/css/main.ec1d8f72.chunk.css"
+  },
+  {
+    "revision": "4417f4c1c33ec024fd02",
+    "url": "/todolist/static/js/main.4417f4c1.chunk.js"
+  },
+  {
+    "revision": "a87775f5415b47ec7ede",
+    "url": "/todolist/static/js/runtime~main.a87775f5.js"
+  },
+  {
+    "revision": "28c3bb1dd88ad9676643",
+    "url": "/todolist/static/js/2.28c3bb1d.chunk.js"
+  },
+  {
+    "revision": "15296ef36116e09beca0a1d613ec02e4",
+    "url": "/todolist/static/media/marble.15296ef3.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todolist/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1144501d9ba04583d2acd4521bc15051",
+    "url": "/todolist/index.html"
+  }
+];
